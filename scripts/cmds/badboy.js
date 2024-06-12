52d943723111df87159258ecb968ef4ceb8f40e9
Djsisjj <114715211+Djsisjj@users.noreply.github.com>
2024-06-12 11:49:54 +0600

    Update and rename ayan.js to badboy.js

--- scripts/cmds/badboy.js
+++ scripts/cmds/badboy.js
@@ -1,8 +1,8 @@
 module.exports = {
     config: {
-        name: "ayan",
+        name: "badboy",
         version: "1.0",
-        author: "MR.AYAN", //** original author fb I'd : https://m.me/MR.AYAN.2X **//
+        author: "MR.AYAN", //** original author fb I'd :  **//
         countDown: 5,
         role: 0,
         shortDescription: "No Prefix",
