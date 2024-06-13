d9cca3d15855b71f2494a6d2f7f77eee1e0cf539
Djsisjj <114715211+Djsisjj@users.noreply.github.com>
2024-06-14 03:47:09 +0600

    Update bot.js

--- scripts/cmds/bot.js
+++ scripts/cmds/bot.js
@@ -2,7 +2,7 @@ module.exports = {
     config: {
         name: "bot",
         version: "1.0",
-        author: "MR.AYAN",//**original anthor fb I'd : https://m.me/MR.AYAN.2X **//
+        author: "MR.AYAN",
         countDown: 5,
         role: 0,
         shortDescription: "No Prefix",
@@ -15,6 +15,6 @@ onChat: async function({
     message,
     getLang
 }) {
-    if (event.body && event.body.toLowerCase() == "bot") return message.reply("Ami bot na amr name mira-!!😑");
+    if (event.body && event.body.toLowerCase() == "bot") return message.reply("Ami bot na amk baby bolba-!!😑");
 }
 }; 
