328038b3d2424282ca6bf4123665d8f36bc93f74
Djsisjj <114715211+Djsisjj@users.noreply.github.com>
2024-06-23 14:48:29 +0600

    Update ss.js

--- scripts/cmds/ss.js
+++ scripts/cmds/ss.js
@@ -1,46 +1,39 @@
-module.exports = {
-    config: {
-        name: 'screenshot',
-        aliases: ['ss'],
-        version: '1.2',
-        author: 'Samir Œ',
-        shortDescription: 'Send a screenshot photo from a URL.',
-        longDescription: 'Sends a screenshot photo from a specified URL.',
-        category: 'owner',
-        guide: {
-            en: '{pn} [URL] | [device]',
-        },
-    },
-    onStart: async function({
-        message,
-        event,
-        args
-    }) {
-      const fuck = args.join(' ');
-      const permission = global.GoatBot.config.DEV;
-      if (!permission.includes(event.senderID)) {
-        api.sendMessage(fuck, event.threadID, event.messageID);
-        return;
-      }
-        try {
-            const argString = args.join(' ');
-            const [providedURL, device] = argString.split('|').map(arg => arg.trim());
-            if (!providedURL) {
-                return message.reply('Please provide a URL.')
-            }
-            const deviceMap = {
-                '1': 'desktop',
-                '2': 'tablet',
-                '3': 'mobile',
-            };
-            const selectedDevice = device && deviceMap[device] ? deviceMap[device] : 'mobile';
-            const api = `https://api-samir.onrender.com/ssweb?url=${encodeURIComponent(providedURL)}&device=${selectedDevice}`;
-            message.reply({
-                attachment: await global.utils.getStreamFromURL(api),
-            })
-        } catch (error) {
-            console.error(error);
-            message.reply('An error occurred while processing the screenshot command.')
-        }
-    },
-}
+const axios = require("axios");
+const baseApiUrl = async () => {
+  const base = await axios.get(
+    `https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`,
+  );
+  return base.data.api;
+};
+
+module.exports.config = {
+  name: "ss",
+  version: "1.0",
+  author: "dipto",
+  role: 2,
+  description: "Take a screenshot of a website",
+  category: "utility",
+  guide: { en: "screenshot [URL]" },
+  coolDowns: 5,
+};
+exports.onStart = async function ({ api, event, args }) {
+  const url = args.join(" ");
+  if (!url) {
+    return api.sendMessage("Please provide a URL.", event.threadID);
+  }
+  try {
+    api.sendMessage(
+      {
+        body: "Screenshot Saved <😽",
+        attachment: await global.utils.getStreamFromURL(
+          `${await baseApiUrl()}/ss?url=${url}`,
+        ),
+      },
+      event.threadID,
+      event.messageID,
+    );
+  } catch (error) {
+    console.error("Error taking screenshot:", error);
+    api.sendMessage("Failed to take a screenshot.", event.threadID);
+  }
+};
