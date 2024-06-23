60a1e5a9f8024c1b1763fc53db33d36316b5035d
Djsisjj <114715211+Djsisjj@users.noreply.github.com>
2024-06-23 19:38:27 +0600

    Update flag.js

--- scripts/cmds/flag.js
+++ scripts/cmds/flag.js
@@ -1,94 +1,105 @@
-const axios = require('axios');
+const axios = require("axios");
+const baseApiUrl = async () => {
+  const base = await axios.get(
+    `https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`,
+  );
+  return base.data.api;
+};
 
 module.exports = {
   config: {
-    name: "country",
-    aliases: ['flag'],
-    version: "1.0",
-    author: "Samir B. Thakuri + OtinXSandip",
-    countDown: 5,
+    name: "flag",
+    aliases: ["flagGame"],
+    version: "3.0",
+    author: "Dipto",
+    countDown: 0,
     role: 0,
-    shortDescription: {
-      en: "Guess the country"
+    description: {
+      en: "Guess the flag name",
     },
-    longDescription: {
-      en: "Guess the country name by its flag"
-    },
-    category: "𝗚𝗔𝗠𝗘",
+    category: "game",
     guide: {
-      en: "{pn}"
+      en: "{pn}",
     },
   },
-
-  onReply: async function ({ args, event, api, Reply, commandName, usersData }) {
-    const { dataGame, country, nameUser } = Reply;
-    if (event.senderID !== Reply.author) return;
-
-    switch (Reply.type) {
-      case "reply": {
-        const userReply = event.body.toLowerCase();
-        if (userReply === country.toLowerCase()) {
-          api.unsendMessage(Reply.messageID).catch(console.error);
-          const rewardCoins = 300;
-          const rewardExp = 10; 
-          const senderID = event.senderID;
-          const userData = await usersData.get(senderID);
-          await usersData.set(senderID, {
-            money: userData.money + rewardCoins,
-            exp: userData.exp + rewardExp,
-            data: userData.data
-          });
-
-          const msg = {
-            body: `✅ ${nameUser}, You've answered correctly!\Answer: ${country}\You've received ${rewardCoins} coins and ${rewardExp} exp as a reward!`
-          };
-          return api.sendMessage(msg, event.threadID, event.messageID);
+  onReply: async function ({ api, event, Reply, usersData }) {
+    const { country, attempts } = Reply;
+    const maxAttempts = 5;
+    if (event.type == "message_reply") {
+      const reply = event.body.toLowerCase();
+      const getCoin = 2 * 120.5;
+      const getExp = 1 * 121;
+      const userData = await usersData.get(event.senderID);
+      if (attempts >= maxAttempts) {
+        await api.sendMessage(
+          "🚫 | You have reached the maximum number of attempts (5).",
+          event.threadID,
+          event.messageID,
+        );
+        return;
+      }
+      if (isNaN(reply)) {
+        if (reply == country.toLowerCase()) {
+          try {
+            await api.unsendMessage(Reply.messageID);
+            await usersData.set(event.senderID, {
+              money: userData.money + getCoin,
+              exp: userData.exp + getExp,
+              data: userData.data,
+            });
+          } catch (err) {
+            console.log("Error: ", err.message);
+          } finally {
+            const message = `✅ | Correct answer!\nYou have earned ${getCoin} coins and ${getExp} exp.`;
+            await api.sendMessage(message, event.threadID, event.messageID);
+          }
         } else {
-          api.unsendMessage(Reply.messageID).catch(console.error);
-          const msg = `${nameUser}, The answer is wrong!!\orrect answer is: ${country}`;
-          return api.sendMessage(msg, event.threadID);
+          Reply.attempts += 1;
+          global.GoatBot.onReply.set(Reply.messageID, Reply);
+          api.sendMessage(
+            `❌ | Wrong Answer.You have ${maxAttempts - Reply.attempts} attempts left.\n✅ | Try Again baby!`,
+            event.threadID,
+            event.messageID,
+          );
         }
       }
     }
   },
 
-  onStart: async function ({ api, event, usersData, commandName }) {
-    const { threadID, messageID } = event;
-    const timeout = 60;
-
+  onStart: async function ({ api, args, event }) {
     try {
-      const response = await axios.get('https://sandipapi.onrender.com/flag');
-      const ansData = response.data;
-      const { link, country } = ansData;
-      const namePlayerReact = await usersData.getName(event.senderID);
-
-      const msg = {
-        body: `What's the name of the country as shown in the flag picture?`,
-        attachment: await global.utils.getStreamFromURL(link)
-      };
-
-      api.sendMessage(msg, threadID, async (error, info) => {
-        if (error) {
-          console.error("Error sending message:", error);
-          return;
-        }
-
-        global.GoatBot.onReply.set(info.messageID, {
-          type: "reply",
-          commandName,
-          author: event.senderID,
-          messageID: info.messageID,
-          dataGame: ansData,
-          country,
-          nameUser: namePlayerReact
-        });
-
-        setTimeout(function () {
-          api.unsendMessage(info.messageID).catch(console.error);
-        }, timeout * 1000);
-      });
+      if (!args[0]) {
+        const response = await axios.get(
+          `${await baseApiUrl()}/flagGame?randomFlag=random`,
+        );
+        const { link, country } = response.data;
+        await api.sendMessage(
+          {
+            body: "Guess this flag name.",
+            attachment: await global.utils.getStreamFromURL(link),
+          },
+          event.threadID,
+          (error, info) => {
+            global.GoatBot.onReply.set(info.messageID, {
+              commandName: this.config.name,
+              type: "reply",
+              messageID: info.messageID,
+              author: event.senderID,
+              link,
+              country,
+              attempts: 0,
+            });
+          },
+          event.messageID,
+        );
+      }
     } catch (error) {
-      console.error("Error occurred:", error);
+      console.error(`Error: ${error.message}`);
+      api.sendMessage(
+        `Error: ${error.message}`,
+        event.threadID,
+        event.messageID,
+      );
     }
-  }
+  },
 };
