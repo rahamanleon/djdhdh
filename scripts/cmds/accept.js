eef8d8fcafeded1ade7251abb41ce62e0d86d53b
rock_x7 <114715211+Djsisjj@users.noreply.github.com>
2024-06-30 11:38:07 +0600

    Update and rename acp.js to accept.js

--- scripts/cmds/accept.js
+++ scripts/cmds/accept.js
@@ -2,7 +2,8 @@ const moment = require("moment-timezone");
 
 module.exports = {
   config: {
-    name: "acp2",
+    name: "accept",
+    aliases: ['acp'],
     version: "1.0",
     author: "Loid Butter",
     countDown: 8,
