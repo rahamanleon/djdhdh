393653cea673a256522ef1b459672348dc793091
rock_x7 <114715211+Djsisjj@users.noreply.github.com>
2024-07-02 01:17:34 +0600

    Update onlywhitelistsThread.js

--- scripts/cmds/onlywhitelistsThread.js
+++ scripts/cmds/onlywhitelistsThread.js
@@ -5,7 +5,7 @@ const { client } = global;
 module.exports = {
   config: {
     name: "whiteliststhreadonly",
-    aliases: ["wlonly", "onlywlstt", "onlywhitelistthread"],
+    aliases: ["wtonly", "onlywlstt", "onlywhitelistthread"],
     version: "1.4",
     author: "NTKhang",
     countDown: 5,
