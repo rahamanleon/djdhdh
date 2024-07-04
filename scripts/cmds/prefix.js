e1a191c0056f6ed7dadf1910156542af66ad1591
rock_x7 <114715211+Djsisjj@users.noreply.github.com>
2024-07-04 12:00:40 +0600

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -16,7 +16,7 @@ module.exports = {
  return message.reply({
  body: `𝗛𝗲𝗹𝗹𝗼 𝗱𝗲𝗮𝗿 🐤\n\n𝗠𝘆 𝗽𝗿𝗲𝗳𝗶𝘅 𝗶𝘀  [ ! ]\n\n______________________________\n\n𝗛𝗢𝗪 𝗧𝗢 𝗨𝗦𝗘?\nexample please type: !help - to view sure cmds\n!owner\n!anivid\n!ping\n\n𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥: 𝗥𝗼𝗖𝗞 𝗘𝘅𝗘 ツ\n______________________________
 `,
-  attachment: await global.utils.getStreamFromURL("https://i.postimg.cc/Qxc3t9rz/download.gif")
+  attachment: await global.utils.getStreamFromURL("https://drive.google.com/uc?export=download&id=1-SGKErjxOtEWsTRDBVyHuysvF9A5fK0d")
  });
  }
  }
