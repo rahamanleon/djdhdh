a1639cb6b227dff370d2c2315969c09587a6b3d9
rock_x7 <114715211+Djsisjj@users.noreply.github.com>
2024-07-07 00:08:55 +0600

    Update horny.js

--- scripts/cmds/horny.js
+++ scripts/cmds/horny.js
@@ -11,11 +11,11 @@ module.exports = {
 	config: {
 		name: "horny",
 		version: "2.0",
-		author: "ROCK",
+		author: "𝗥𝗼𝗖𝗞 𝗘𝘅𝗘",
 		countDown: 30,
 		role: 2,
 		shortDescription: "",
-		longDescription: "get kanda/p***n video hilake sojaa",
+		longDescription: "horny romantic video",
 		category: "18+",
 		guide: "{p}{n}",
 	},
@@ -29,29 +29,54 @@ module.exports = {
 			body: "⏳| Please Wait...",
 		});
 
-		const link = [
-			"https://drive.google.com/uc?export=download&id=1BOVPMYkWg5ocM6YgFJzaKlGVpPnjq2NT",
+	const link = [
+
+		
+       "https://drive.google.com/uc?export=download&id=1BOVPMYkWg5ocM6YgFJzaKlGVpPnjq2NT",
       "https://drive.google.com/uc?export=download&id=18u9GDdCkMQacxDHSFiMVyoGNVo2Wbi4R",
       "https://drive.google.com/uc?export=download&id=1BGlba3HxCt-mMQx_-lyM1cJ5PBlPaB6-",
       "https://drive.google.com/uc?export=download&id=19hslJln0pu0mYZHW831wohDgtSoMWXOK",
       "https://drive.google.com/uc?export=download&id=19sVkUBB8nH2BuA7fXsMEltfGMWQeXYH-",
       "https://drive.google.com/uc?export=download&id=1BhsTRWlrn7q-vAQwPRfZ-aLYgiGlognp",
       "https://drive.google.com/uc?export=download&id=18hPtMit7wVuGAtIHBwFswQgZ-MpJwF2v",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-		];
+       "https://drive.google.com/uc?export=download&id=1Ag-htOQfw-5jrlDefAx6ELo2cZXK0wyp",
+	"https://drive.google.com/uc?export=download&id=1Arqydz5yfa-mpPZ_nLh6vKzXmAKYGGEK",
+	"https://drive.google.com/uc?export=download&id=1D47HAjr4pLBgpd6tY_TDquN1UwDqUEcp",	
+	"https://drive.google.com/uc?export=download&id=1Che6EVdtCLGAXzJH3HS5IYqamUJWcJbu",	
+       "https://drive.google.com/uc?export=download&id=1BulVTvBIhDjKwCROvx3z91nCnUWv_NoB",
+       "https://drive.google.com/uc?export=download&id=1Ab0iktkVs8wsE3Zhc7LnzJANMdJVsBIN",
+       "https://drive.google.com/uc?export=download&id=19i0kk8hXtzA4Q8fPXR7YgKriWuMDZHVh",
+       "https://drive.google.com/uc?export=download&id=1COzE7QvP_8S1Old53aO9yaUGOIpNeEub",
+       "https://drive.google.com/uc?export=download&id=1CSo_Q7QFUIw0ZsS5hwugizduJap7xREp",
+       "https://drive.google.com/uc?export=download&id=1BD6DjwPKuP98__dagH8cjvunE68blRm-", 
+      "https://drive.google.com/uc?export=download&id=1BkHaqQ-FM8Y6bX8kNaYpQeJhStvJMUM8", 
+      "https://drive.google.com/uc?export=download&id=1CY720i7LPwsKSXoQq_dCCEjKQhOhqqoI",
+      "https://drive.google.com/uc?export=download&id=19Jgz2i8-JggQpx88YwvUkbOVCSENOMCw",
+		 "https://drive.google.com/uc?export=download&id=1AL3JpGsEt14M1TauptQEkgFC1iAXvpM3",
+		 "https://drive.google.com/uc?export=download&id=1BF9ftMKptzHQqXGCdYKdsPd_LQu2XukX",
+		 "https://drive.google.com/file/d/1D4mpqcpebNrpVsa1w2G6GrNWEyuy_DyY/view?usp=drivesdk",
+		 "https://drive.google.com/uc?export=download&id=1CX-t1Icl_D0dKd1J6YwNOfdV82shJNgS",
+		 "https://drive.google.com/uc?export=download&id=1BY_qhPDeZDH8SPWJuYMJnGVxU0ZYwfXp",
+		 "https://drive.google.com/uc?export=download&id=18sZLYcYZuBRsE408ZWq90kIxvNZ9_6os",
+		 "https://drive.google.com/uc?export=download&id=19nUg6XzWLIeoWD9BeXAUN3JD0fA6fbgs",
+		 "https://drive.google.com/file/d/1ChY43755H_QJHmceSnMmyztLH2-Lay36/view?usp=drivesdk",
+		 "https://drive.google.com/uc?export=download&id=19u2OstXogVtL379KlC2LhoMHbgPCLSvw",
+		 "https://drive.google.com/uc?export=download&id=19OW963-Y_1n62W4vfe0YXGo32QdqF7JI",
+		 "https://drive.google.com/uc?export=download&id=1C8eJIs9xXcbzKhJFVAX1piPxFuWuzC_h",
+		 "https://drive.google.com/uc?export=download&id=18wls5bT8bPvBrTNVRZbUqLfLYdC7djyC",
+		 "https://drive.google.com/uc?export=download&id=1B1h8JsFTYJqeoBWxwVMCC2diKLdWZGnz",
+
+	
+
+		
+
+
+
+
+
+		
+		
+	];
 
 		const availableVideos = link.filter(video => !this.sentVideos.includes(video));
 
@@ -66,7 +91,7 @@ module.exports = {
 
 		if (senderID !== null) {
 			message.reply({
-				body: '✅𝙝𝙚𝙧𝙚 𝙮𝙤𝙪𝙧 𝙝𝙤𝙧𝙣𝙮 𝙫𝙞𝙙𝙚𝙤 𝙗𝙖𝙗𝙮',
+				body: '𝙝𝙤𝙧𝙣𝙮 𝙫𝙞𝙙𝙚𝙤 𝙗𝙖𝙗𝙮 <🥵',
 				attachment: await global.utils.getStreamFromURL(randomVideo),
 			});
 
