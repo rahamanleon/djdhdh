f01f12f14f59a002e6e7dc9cbc72921a725a0ce0
rock_x7 <114715211+Djsisjj@users.noreply.github.com>
2024-07-07 11:43:15 +0600

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -14,10 +14,10 @@ module.exports = {
  onChat: async function({ event, message, getLang }) {
  if (event.body && event.body.toLowerCase() === "prefix") {
  return message.reply({
- body: `𝗛𝗲𝗹𝗹𝗼 𝗱𝗲𝗮𝗿 🐤\n\n𝗠𝘆 𝗽𝗿𝗲𝗳𝗶𝘅 𝗶𝘀  [ ! ]\n\n______________________________\n\n𝗛𝗢𝗪 𝗧𝗢 𝗨𝗦𝗘?\nexample please type: !help - to view sure cmds\n!owner\n!anivid\n!ping\n\n𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥: 𝗥𝗼𝗖𝗞 𝗘𝘅𝗘 ツ\n______________________________
+ body: `𝗛𝗲𝗹𝗹𝗼 𝗱𝗲𝗮𝗿 🐤\n\n𝗠𝘆 𝗽𝗿𝗲𝗳𝗶𝘅 𝗶𝘀  [ ! ]\n\n______________________________\n\n𝗛𝗢𝗪 𝗧𝗢 𝗨𝗦𝗘?\nexample please type: !help - to view sure cmds\n!owner\n!info\n!anime\n!ping\n\n𝗕𝗔𝗕𝗬 𝗢𝗪𝗡𝗘𝗥: 𝗥𝗼𝗖𝗞 𝗘𝘅𝗘 ツ\n______________________________
 `,
   attachment: await global.utils.getStreamFromURL("https://drive.google.com/uc?export=download&id=1-SGKErjxOtEWsTRDBVyHuysvF9A5fK0d")
  });
  }
  }
-} 
+}
