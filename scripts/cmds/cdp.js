a9a815b4118a8ecb87b956cccc15eea382a7fb2d
rock_x7 <114715211+Djsisjj@users.noreply.github.com>
2024-07-31 02:44:26 +0600

    Create cdp.js

---
+++ scripts/cmds/cdp.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = './cdp.json'; 
+
+module.exports = {
+	config: {
+		name: "copuledp",
+		aliases: ["cdp","pairdp"],
+		version: "1.0",
+		author: "𝗕𝗔𝗗𝗕𝗢𝗬 彡",
+		countDown: 2,
+		role: 0,
+		shortDescription: "pairdp",
+		longDescription: "cdp",
+		category: "image",
+		guide: "{pn}"
+	}, 
+
+	onStart: async function ({ message }) {
+		let pairs = JSON.parse(fs.readFileSync(path, 'utf8'));
+
+		const selectedPair = pairs[Math.floor(Math.random() * pairs.length)]; 
+
+		const maleLink = selectedPair.male;
+		const femaleLink = selectedPair.female;
+		let attachments = [];
+		attachments.push(await global.utils.getStreamFromURL(maleLink));
+		attachments.push(await global.utils.getStreamFromURL(femaleLink));
+		message.send({
+			body: 'cdp',
+			attachment: attachments
+		});
+	}
+};
